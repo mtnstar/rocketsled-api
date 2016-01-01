bcfccb724e2a69f45772533a92295ae8feb5ab6b
pascal simon <ps@s-it.ch>
2016-01-01 18:23:23 +0100

    disable users controller for the moment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,59 +1,59 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  #before_action :set_user, only: [:show, :update, :destroy]
 
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all
+  ## GET /users
+  ## GET /users.json
+  #def index
+    #@users = User.all
 
-    render json: @users
-  end
+    #render json: @users
+  #end
 
-  # GET /users/1
-  # GET /users/1.json
-  def show
-    render json: @user
-  end
+  ## GET /users/1
+  ## GET /users/1.json
+  #def show
+    #render json: @user
+  #end
 
-  # POST /users
-  # POST /users.json
-  def create
-    @user = User.new(user_params)
+  ## POST /users
+  ## POST /users.json
+  #def create
+    #@user = User.new(user_params)
 
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
+    #if @user.save
+      #render json: @user, status: :created, location: @user
+    #else
+      #render json: @user.errors, status: :unprocessable_entity
+    #end
+  #end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
-  def update
-    @user = User.find(params[:id])
+  ## PATCH/PUT /users/1
+  ## PATCH/PUT /users/1.json
+  #def update
+    #@user = User.find(params[:id])
 
-    if @user.update(user_params)
-      head :no_content
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
+    #if @user.update(user_params)
+      #head :no_content
+    #else
+      #render json: @user.errors, status: :unprocessable_entity
+    #end
+  #end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user.destroy
+  ## DELETE /users/1
+  ## DELETE /users/1.json
+  #def destroy
+    #@user.destroy
 
-    head :no_content
-  end
+    #head :no_content
+  #end
 
-  private
+  #private
 
-    def set_user
-      @user = User.find(params[:id])
-    end
+    #def set_user
+      #@user = User.find(params[:id])
+    #end
 
-    def user_params
-      params.require(:user).permit(:first_name, :email)
-    end
+    #def user_params
+      #params.require(:user).permit(:first_name, :email)
+    #end
 end
